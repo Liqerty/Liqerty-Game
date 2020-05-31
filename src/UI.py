64548104eac2ed0836b16826266f156415fd5a77
KseandI <KseandI@gmail.com>
2020-05-31 12:10:04 +0300

    tried to add append func

--- src/UI.py
+++ src/UI.py
@@ -13,12 +13,15 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(597, 408)
+        MainWindow.resize(800, 600)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(270, 180, 41, 41))
+        self.label.setGeometry(QtCore.QRect(270, 180, 40, 40))
         self.label.setObjectName("label")
+        self.btnTest = QtWidgets.QPushButton(self.centralwidget)
+        self.btnTest.setGeometry(QtCore.QRect(280, 400, 94, 36))
+        self.btnTest.setObjectName("btnTest")
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
@@ -28,3 +31,4 @@ class Ui_MainWindow(object):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.label.setText(_translate("MainWindow", "TextLabel"))
+        self.btnTest.setText(_translate("MainWindow", "testBtn"))
--- src/main.py
+++ src/main.py
@@ -1,17 +1,25 @@
 from src.UI import Ui_MainWindow
-import PyQt5
+from PyQt5 import QtWidgets
+from PyQt5 import QtCore
 import sys
 
 
-class Window(PyQt5.QtWidgets.QMainWindow):
+class Window(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
-        PyQt5.QtWidgets.QWidget.__init__(self, parent)
+        QtWidgets.QWidget.__init__(self, parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.ui.btnTest.clicked.connect(self.addWidget)
+
+    def addWidget(self):
+        test = QtWidgets.QLabel(text="Test", parent=self.centralWidget())
+        test.setGeometry(QtCore.QRect(170, 280, 40, 40))
+        test.setObjectName("test")
+        print(test)
 
 
 if __name__ == "__main__":
-    app = PyQt5.QtWidgets.QApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)
     window = Window()
     window.show()
     app.exec_()
